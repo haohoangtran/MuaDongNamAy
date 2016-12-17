f4463c4009bb5fe17ca8803645a3ea637e87d4b7
DESKTOP-M0ENNDH\Khuong Duy <duyakse04298@fpt.edu.vn>
2016-12-17 13:12:26 +0700

    add model, view,

--- MuaDongNamAy/src/views/View.java
+++ MuaDongNamAy/src/views/View.java
@@ -1,7 +1,28 @@
 package views;
 
+import models.Model;
+
+import java.awt.*;
+
 /**
  * Created by DUC THANG on 12/17/2016.
  */
 public class View {
+    private Image image;
+
+    public View(Image image) {
+        this.image = image;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public void draw(Graphics g, Model model){
+        g.drawImage(image,model.getX(),model.getY(),model.getWidth(),model.getHeight(),null);
+    }
 }
